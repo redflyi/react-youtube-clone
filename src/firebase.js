9718ef5bdb3456f79a26beedc2b5705652a60582
redflyi <ikem007@hotmail.com>
2021-04-30 17:29:18 +0100

    First Commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCysrr7muvEOzdl8oA_SOTuZ_YeibaYZik",
+  authDomain: "vidtube-007.firebaseapp.com",
+  projectId: "vidtube-007",
+  storageBucket: "vidtube-007.appspot.com",
+  messagingSenderId: "895490316994",
+  appId: "1:895490316994:web:9aaff84cbe5c40c161f95b",
+  measurementId: "G-E4FEEE1K7S"
+}
